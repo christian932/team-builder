dbd6f63e6311d272b1ada9b0df72e5c1ad5e79f8
christian932 <christianfarnsworth@gmail.com>
2021-06-03 18:39:14 -0700

    not working

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import TeamMemberForm from './components/TeamMemberForm';
+import TeamMember from './components/TeamMember';
+
+
+const initialFormValues = {
+  username: '',
+  email: '',
+  role: '',
+}
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue})
+  }
+
+  const submitForm = () => {
+    const newTeamMember = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    if (!newTeamMember.username || !newTeamMember.email || !newTeamMember.role) return
+    // axios.post('fakeapi.com', newTeamMember)
+    //   .then(res => {
+    //     const teamMemberFromBackend = res.data
+        setTeamMembers([newTeamMember, ...teamMembers])
+        setFormValues(initialFormValues)
+  }      
+  //     })
+  // }
+
+  // useEffect(() => {
+  //   axios.get('fakeapi.com')
+  //   .then(res => setTeamMembers(res.data))
+  // }, [])
+
+
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Team Syntax Error</h1>
       </header>
+      <TeamMemberForm
+      update={updateForm}
+      submit={submitForm}
+      values={formValues}
+      />
+
+      { 
+        teamMembers.map(teamMember => {
+          return (
+            <TeamMember key={teamMember.id} details={teamMember} />
+          )
+        })}
     </div>
   );
 }
